bd6c45ddd6761d951672adc621e859a4fd28c5a0
arbaz98 <arbaz.k480@yahoo.com>
2025-02-25 17:17:40 +0500

    design modification

--- tilt-setup.js
+++ tilt-setup.js
@@ -1,12 +1,10 @@
-const tiltItems = document.querySelectorAll('.services-container .item');
-
-console.log(tiltItems)
-const tiltConfig = {
-    max: 6,
-    speed: 10,
-    transition: true,
-    reverse: true
-}
-tiltItems.forEach(item => {
-    VanillaTilt.init(item, tiltConfig)
-})
+// const tiltItems = document.querySelectorAll('.services-container .item');
+// const tiltConfig = {
+//     max: 6,
+//     speed: 10,
+//     transition: true,
+//     reverse: true
+// }
+// tiltItems.forEach(item => {
+//     VanillaTilt.init(item, tiltConfig)
+// })
